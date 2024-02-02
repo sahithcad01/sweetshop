337ca7a7e412e0cb38c0a57e29f361dde5b492b0
sahithcad01 <sahithcad01@gmail.com>
2024-02-02 18:04:00 +0530

    p.menu html files added

--- js/script.js
+++ js/script.js
@@ -1,102 +1,102 @@
-var a=10;
-var b=20;
+// var a=10;
+// var b=20;
 
-function kalyanam(a,b,c){
-    /* var a=10;
-    var b=20;
-    var c=30; */
-    mehandi();
-    function mehandi(){
-        console.log("You are welcome to the Mehandi Function");
-        d=a+b+c;
-        console.log("d is "+d);
-    }
+// function kalyanam(a,b,c){
+//     /* var a=10;
+//     var b=20;
+//     var c=30; */
+//     mehandi();
+//     function mehandi(){
+//         console.log("You are welcome to the Mehandi Function");
+//         d=a+b+c;
+//         console.log("d is "+d);
+//     }
    
-    console.log(a*b*c);
+//     console.log(a*b*c);
   
-}
-kalyanam(10,20,30);
+// }
+// kalyanam(10,20,30);
 
-function hello()
-{
-    let b=5;
-    let c=a+b;
-    console.log(a%b);
-}
-hello();
+// function hello()
+// {
+//     let b=5;
+//     let c=a+b;
+//     console.log(a%b);
+// }
+// hello();
 
-console.log((a<b)?"True":"False");
+// console.log((a<b)?"True":"False");
 
-if(a<=b){
-console.log("True");
-}
+// if(a<=b){
+// console.log("True");
+// }
 
-if(a>b && b>a || a==10){
-    console.log("Success");
-}
-else{
-    console.log("Sethuru");
-}
+// if(a>b && b>a || a==10){
+//     console.log("Success");
+// }
+// else{
+//     console.log("Sethuru");
+// }
 
-function operators()
-{
-    var a=parseInt(prompt("Enter value of A: "));
-    var b=parseInt(prompt("Enter value of B: "));
+// function operators()
+// {
+//     var a=parseInt(prompt("Enter value of A: "));
+//     var b=parseInt(prompt("Enter value of B: "));
 
-    function addition(){
-        return(a+b);
-    }
-    console.log(addition());
+//     function addition(){
+//         return(a+b);
+//     }
+//     console.log(addition());
 
-    console.log(a-b);
-    console.log(a*b);
-    console.log(a/b);
-    console.log(a%b);
-    console.log(a**b);
-    console.log(a^b);
-    console.log(a>b);
-    console.log(a<b);
+//     console.log(a-b);
+//     console.log(a*b);
+//     console.log(a/b);
+//     console.log(a%b);
+//     console.log(a**b);
+//     console.log(a^b);
+//     console.log(a>b);
+//     console.log(a<b);
 
-}
-operators();
+// }
+// operators();
 
-function ifdemo(){
-    var age =parseInt(prompt("Enter your age: "));
-    Eli_age =18;
-    if(age>=Eli_age){
-        console.log("You are eligible for Vote");
-    }
-    else{
-        console.log("You are not Eligible for Vote");
-    }
+// function ifdemo(){
+//     var age =parseInt(prompt("Enter your age: "));
+//     Eli_age =18;
+//     if(age>=Eli_age){
+//         console.log("You are eligible for Vote");
+//     }
+//     else{
+//         console.log("You are not Eligible for Vote");
+//     }
     
-}
+// }
 
-ifdemo();
+// ifdemo();
 
-function ifelsedemo()
-{
-    a=100;
-    b=50;
-    c=2000;
-    if(a>b && a>c){
-        console.log("a");
-    }
-    else if(b>a && b>c) {
-        console.log("b");
-    }
-    else {
-        console.log("c");
-    }
-}
-ifelsedemo();
+// function ifelsedemo()
+// {
+//     a=100;
+//     b=50;
+//     c=2000;
+//     if(a>b && a>c){
+//         console.log("a");
+//     }
+//     else if(b>a && b>c) {
+//         console.log("b");
+//     }
+//     else {
+//         console.log("c");
+//     }
+// }
+// ifelsedemo();
 
-const cardemo={
-    car_type:"p",
-    car_model:"indica",
-    car_Version:"BS6"
-};
+// const cardemo={
+//     car_type:"p",
+//     car_model:"indica",
+//     car_Version:"BS6"
+// };
 
-cardemo.car_model="Sumo";
-console.log(cardemo);
+// cardemo.car_model="Sumo";
+// console.log(cardemo);
 
